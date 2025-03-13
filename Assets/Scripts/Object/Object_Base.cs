16421bced8e443e60bd78552b5eb77bdf0e9fbc9
bbpasd <bbpasd@gmail.com>
2025-03-14 08:30:05 +0900

    몬스터에 HP 바 추가

--- Assets/Scripts/Object/Object_Base.cs
+++ Assets/Scripts/Object/Object_Base.cs
@@ -1,17 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-/// <summary> 모든 오브젝트들의 베이스. 스탯 등 공용 정보를 가짐 </summary>
+/// <summary> Base of ALL Object </summary>
 public class Object_Base : MonoBehaviour
 {
     // 각종 스탯
     public float currentHp;
     public float maxHp = 10.0f;
     public float attackPower = 10.0f;
+    public bool isAttackedOnShot = false;
+    public GameObject hpPanel;
+    public Slider hpSlider;
 
     protected void Start() {
         currentHp = maxHp;
+
+        hpPanel = transform.Find("HPPanel")?.gameObject;
+
     }
 
     public virtual void OnAttack(float amount) {
@@ -23,6 +30,12 @@ public class Object_Base : MonoBehaviour
             return;
         }
 
+        if (!isAttackedOnShot) {
+            isAttackedOnShot = true;
+            hpPanel.SetActive(true);
+            hpSlider = hpPanel.transform.Find("Canvas")?.Find("HpSlider")?.GetComponent<Slider>();
+            
+        }
         UpdateHpBar();
     }
 
@@ -31,6 +44,8 @@ public class Object_Base : MonoBehaviour
     }
 
     public virtual void UpdateHpBar() {
+        if (hpSlider == null) return;
 
+        hpSlider.value = currentHp / maxHp;
     }
 }
